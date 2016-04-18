e4bdddf733a40a8bd14032fb9fc45b32305bb64e
Shivang Shah <shivang_shah@gap.com>
2016-04-18 11:59:41 -0700

    Testing with brixton RC2 release. Everything working as expected!

--- profile-mock-server/src/main/java/com/shivang/performance/PingController.java
+++ profile-mock-server/src/main/java/com/shivang/performance/PingController.java
@@ -5,24 +5,36 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.cloud.sleuth.SpanNamer;
+import org.springframework.cloud.sleuth.TraceKeys;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
 
 @RestController
 public class PingController {
 
     @Autowired
     private TaskServiceImpl taskService;
+
+    @Autowired
+    private Tracer tracer;
+    @Autowired
+    private TraceKeys traceKeys;
+    @Autowired
+    private SpanNamer spanNamer;
     private static final Logger LOGGER = LoggerFactory.getLogger(PingController.class);
 
     @RequestMapping("/ping")
     public DeferredResult<Map<String, Boolean>> ping() {
         DeferredResult<Map<String, Boolean>> deferredResult = new DeferredResult<>();
-        CompletableFuture.supplyAsync(taskService::execute)
+        CompletableFuture.supplyAsync(() -> taskService.execute(),
+                new TraceableExecutorService(Executors.newFixedThreadPool(5), tracer, traceKeys, spanNamer))
                 .whenCompleteAsync((result, throwable) -> deferredResult.setResult(result));
         LOGGER.warn("Servlet thread released");
 
